af4c2cf1f44eccb244558bec4b8b93a37e1e06e1
Matheus-Dev <darkaixxt@gmail.com>
2023-06-18 19:54:51 -0300

    Cria caso de uso para enviar e remover pedidos dos motoristas

--- delivery/core/usecases/pedidos.py
+++ delivery/core/usecases/pedidos.py
@@ -4,6 +4,7 @@ from delivery.core.utils import dictfetchall
 
 class CasePedidos():
 
+
     def get_all(self, date, status_pedido, forma_pagamento):
         """ BUSCA TODOS OS PEDIDOS """
 
@@ -61,7 +62,8 @@ class CasePedidos():
                        #e.cep, e.cidade, e.estado,
                        e.logradouro, e.numLogr, e.complLogr, e.pontoreferencia,
                        b.nome as "bairro"
-                  FROM pedido p 
+                  #FROM pedido p 
+                  FROM gtech_pedidos p 
                   JOIN pedido_produto pp
                     ON p.id = pp.idPedido 
                   JOIN produto p2 
@@ -83,3 +85,96 @@ class CasePedidos():
             data = dictfetchall(cursor)
 
         return data if data else [] 
+
+    def get_entrega(self, date):
+        """ BUSCA OS PEDIDOS EM ROTA DE ENTREGA """
+
+        with connections["default"].cursor() as cursor:
+
+            _sql = f"""
+                SELECT * FROM pedido_entrega
+                 WHERE data = '{date}';
+            """
+
+            cursor.execute(_sql)
+            data = dictfetchall(cursor)
+
+        return data if data else [] 
+
+    def envia_pedido(self, payload):
+        """ ADICIONA PEDIDO PARA ROTA DE ENTREGA """
+
+        sql_insert = f"""
+            INSERT INTO pedido_entrega (idPedido, data, hora, status, cpf_motorista, motorista, cpf_user, usuario)
+                 VALUES ({payload['idPedido']}, '{payload['data']}', '{payload['hora']}', '{payload['status']}',
+                         {payload['cpf_motorista']}, '{payload['motorista']}', {payload['cpf_user']}, '{payload['usuario']}');
+        """
+
+        sql_update = f"""
+            UPDATE gtech_pedidos gped
+               SET gped.status = 'ENVIADO'
+             WHERE gped.id = {payload['idPedido']};
+        """
+
+        try:
+            with connections["default"].cursor() as cursor:
+                cursor.execute(sql_insert)
+
+                if cursor.rowcount == 1:
+                    cursor.execute(sql_update)
+
+                    if cursor.rowcount == 1:
+                        connections["default"].commit()
+
+                    else:
+                        connections["default"].rollback()
+                        return {'success': False, 'message': 'operacao falho ao realizar o update!'} 
+
+                else:
+                    connections["default"].rollback()
+                    return {'success': False, 'message': 'operacao falho ao realizar o insert!'} 
+
+        except Exception as e:
+            connections["default"].rollback()
+            return f'Erro: {str(e)}'
+
+        return {'success': True, 'message': 'operacao realizada com sucesso!'} 
+
+    def remove_pedido(self, payload):
+        """ REMOVE PEDIDO DE ROTA ENTREGA """
+
+        sql_insert = f"""
+            INSERT INTO pedido_entrega (idPedido, data, hora, status, cpf_motorista, motorista, cpf_user, usuario)
+                 VALUES ({payload['idPedido']}, '{payload['data']}', '{payload['hora']}', '{payload['status']}',
+                         {payload['cpf_motorista']}, '{payload['motorista']}', {payload['cpf_user']}, '{payload['usuario']}');
+        """
+
+        sql_update = f"""
+            UPDATE gtech_pedidos gped
+               SET gped.status = 'EM_ABERTO'
+             WHERE gped.id = {payload['idPedido']};
+        """
+
+        try:
+            with connections["default"].cursor() as cursor:
+                cursor.execute(sql_insert)
+
+                if cursor.rowcount == 1:
+                    cursor.execute(sql_update)
+
+                    if cursor.rowcount == 1:
+                        connections["default"].commit()
+
+                    else:
+                        connections["default"].rollback()
+                        return {'success': False, 'message': 'operacao falho ao realizar o update!'}
+
+                else:
+                    connections["default"].rollback()
+                    return {'success': False, 'message': 'operacao falho ao realizar o insert!'}
+
+        except Exception as e:
+            connections["default"].rollback()
+            return f'Erro: {str(e)}'
+
+        return {'success': True, 'message': 'operacao realizada com sucesso!'}
