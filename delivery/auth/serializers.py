14fa454bcc59c082a274cbfe91fa6b4fe4852e05
Matheus-Dev <darkaixxt@gmail.com>
2023-06-17 23:47:42 -0300

    Aumenta o tamanho da propriedade do CPF para 11 digitos

--- delivery/auth/serializers.py
+++ delivery/auth/serializers.py
@@ -29,7 +29,7 @@ class LoginSerializer(TokenObtainPairSerializer):
 
 class RegistrationSerializer(UserSerializer):
     password = serializers.CharField(max_length=128, min_length=8, write_only=True, required=True)
-    cpf = serializers.DecimalField(required=True, write_only=True, decimal_places=0, max_digits=8)
+    cpf = serializers.DecimalField(required=True, write_only=True, decimal_places=0, max_digits=11)
 
     class Meta:
         model = User
