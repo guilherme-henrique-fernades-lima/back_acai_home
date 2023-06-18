e9cfcef2f79bfa429e1a767be47f465829256420
Matheus-Dev <darkaixxt@gmail.com>
2023-06-18 12:26:33 -0300

    Cria novo EndPoint para listagem de pedidos pendentes

--- delivery/core/views/pedidos.py
+++ delivery/core/views/pedidos.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from rest_framework import viewsets, status
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework.decorators import action
 from rest_framework.permissions import AllowAny, IsAuthenticated
 
 from delivery.core.models import Pedidos
@@ -41,6 +42,24 @@ class PedidosViewSet(viewsets.ModelViewSet):
             new_data = {'data': data, 'status': contagem_status.to_dict()}
 
             return Response(data=new_data, status=status.HTTP_200_OK)
+
+        except Exception as err:
+            print("ERROR>>>", err)
+            return Response(data={'success': False, 'message': err}, status=status.HTTP_400_BAD_REQUEST)
+
+    @action(detail=False, methods=['get'], url_path='pendentes')
+    def pendentes(self, request):
+
+        date = request.GET.get("date", datetime.now().date())
+
+        try:
+            pedido = CasePedidos()
+            data = pedido.get_open_orders(date)
+
+            if not data:
+                return Response(data={'success': False, 'message': 'nenhum pedido encontrado.'}, status=status.HTTP_404_NOT_FOUND)
+
+            return Response(data=data, status=status.HTTP_200_OK)
 
         except Exception as err:
             print("ERROR>>>", err)
