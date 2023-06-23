328e9127db4f57b0977a3bd98bc1b1e8b439e651
Matheus-Dev <darkaixxt@gmail.com>
2023-06-22 21:13:40 -0300

    Permite o allow hosts na url de deploy

--- delivery/settings.py
+++ delivery/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-%h4o++jrg$wm*!eu+pbpn!qj-@+@r#=+6am%o!u9k5wdxj=zv$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [back-acai-home-1d182e2feaa2.herokuapp.com]
+ALLOWED_HOSTS = ['back-acai-home-1d182e2feaa2.herokuapp.com']
 
 
 # Application definition
