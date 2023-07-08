bcc7ae0bb37e487dae04fd850b42cbb15931d272
glayson visgueira <glaysonwow@gmail.com>
2023-07-07 22:44:26 -0300

    Pandas para totalizar relatorios de entregas

--- delivery/core/views/pedidos.py
+++ delivery/core/views/pedidos.py
@@ -18,7 +18,7 @@ from delivery.core.utils import dispatch_event_socket
 class PedidosViewSet(viewsets.ModelViewSet):
     queryset = Pedidos.objects.all()
     serializer_class = PedidosMS
-    permission_classes = (IsAuthenticated,)
+    permission_classes = (IsAuthenticated,) 
 
     def get_serializer_class(self):
         serializer = super().get_serializer_class()
@@ -151,9 +151,9 @@ class PedidosViewSet(viewsets.ModelViewSet):
             return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
 
     @action(detail=False, methods=['post'], url_path='concluir')
-    def concluir_pedido(self, request):
+    def concluir_pedido(self, request):        
 
-        data = request.data
+        data = request.data        
 
         try:
             if data:
@@ -184,6 +184,8 @@ class PedidosViewSet(viewsets.ModelViewSet):
     @action(detail=False, methods=['get'], url_path='concluidos')
     def pedidos_concluidos(self, request):
 
+       
+
         date = request.GET.get("date", datetime.now().date())
         forma_pagamento = request.GET.get("tp_pag")
         motorista = request.GET.get("cpf_motorista")
@@ -192,8 +194,53 @@ class PedidosViewSet(viewsets.ModelViewSet):
             pedido_rep = RepoPedidos()
             response = pedido_rep.get_pedidos_concluidos(date, forma_pagamento, motorista)
 
-            return Response(data=response, status=status.HTTP_200_OK)
+            df_pedidos = pd.DataFrame(response)          
+
+            formas_pagamento = df_pedidos['formaPagamento'].value_counts()           
+            total_pedidos = df_pedidos['idPedido'].count()
+            valor_total_produtos =  df_pedidos['valor'].sum() - df_pedidos['taxaentrega'].sum()
+            valor_total_pedidos =  df_pedidos['valor'].sum()
+            valor_total_taxa_entrega =  df_pedidos['taxaentrega'].sum()   
+
+            payload = {
+                'indicadores': {
+                    'formas_pagamento': {
+                      'pix': formas_pagamento.get("PIX", 0),  
+                      'dinheiro': formas_pagamento.get("DINHEIRO", 0),  
+                      'credito': formas_pagamento.get("CREDITO", 0),  
+                      'debito': formas_pagamento.get("DEBITO", 0),  
+                      'dinheiro_cartao': formas_pagamento.get("DINHEIRO_CARTAO", 0),  
+                    },                    
+                    'total_pedidos': total_pedidos,
+                    'valor_total_produtos': valor_total_produtos,
+                    'valor_total_pedidos': valor_total_pedidos,
+                    'valor_total_taxa_entrega': valor_total_taxa_entrega
+                },
+                'data': response
+            }               
+
+            return Response(data=payload, status=status.HTTP_200_OK)
 
         except Exception as err:
             print("ERROR>>>", err)
-            return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+            payload_error =  {
+                'indicadores': {
+                    'formas_pagamento': {
+                      'pix': 0,  
+                      'dinheiro': 0,  
+                      'credito': 0,  
+                      'debito': 0,  
+                      'dinheiro_cartao': 0,  
+                    },                    
+                    'total_pedidos': 0,
+                    'valor_total_produtos': 0,
+                    'valor_total_pedidos': 0,
+                    'valor_total_taxa_entrega': 0
+                },
+                'data': []
+            } 
+
+            return Response(data=payload_error, status=status.HTTP_400_BAD_REQUEST)
+            
+           #return Response(data={'success': False, 'message': str(err)}, status=status.HTTP_400_BAD_REQUEST)
